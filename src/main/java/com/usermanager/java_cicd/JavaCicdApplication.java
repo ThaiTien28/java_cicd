bd4554b0954a1701e37cf973e45bf3e0f267d476
TienT <tienthai0428@gmail.com>
2026-01-21 16:30:56 +0700

    init

---
+++ src/main/java/com/usermanager/java_cicd/JavaCicdApplication.java
@@ -0,0 +1,13 @@
+package com.usermanager.java_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCicdApplication.class, args);
+    }
+
+}
